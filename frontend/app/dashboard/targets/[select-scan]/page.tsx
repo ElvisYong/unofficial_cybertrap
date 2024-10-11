d53d8ded0209e3b0450dac017d754db1df349e2e
Wei Lun Teo <weilun.teo.2021@scis.smu.edu.sg>
2024-10-11 19:23:41 +0800

    fix: scandate Jan 1,1 to N/A for non-completed scans

--- frontend/app/dashboard/targets/[select-scan]/page.tsx
+++ frontend/app/dashboard/targets/[select-scan]/page.tsx
@@ -22,8 +22,7 @@ interface Template {
 export default function SelectScan() {
     const [templates, setTemplates] = useState<Template[]>([]);
     const [selectedTemplates, setSelectedTemplates] = useState<string[]>([]);
-    const [scanAllTemplates, setScanAllTemplates] = useState(false); 
-    const [scanAllTemplatesID, setScanAllTemplatesID] = useState<string>("");
+    const [scanAllTemplates, setScanAllTemplates] = useState(false);
     const [target, setTarget] = useState("");
     const router = useRouter();
     const { toast } = useToast();
@@ -39,14 +38,14 @@ export default function SelectScan() {
             .then(response => response.json())
             .then(data => setTemplates(data))
             .catch(error => {
-                console.log('Error fetching templates:', error);
+                console.error('Error fetching templates:', error);
                 toast({
                     title: "Error",
                     description: "Failed to fetch templates. Please try again.",
                     variant: "destructive",
                 });
             });
-    }, []); 
+    }, [toast]); 
 
     const handleTemplateSelection = (templateId: string) => {
         setSelectedTemplates(prev => 
@@ -59,9 +58,8 @@ export default function SelectScan() {
     const handleSubmit = async (event: React.FormEvent) => {
         event.preventDefault();
     
-        const domainId = target; // Get the target from the URL
+        const domainId = target;
     
-        // Check if we have a valid domainId
         if (!domainId) {
             toast({
                 title: "Error",
@@ -71,13 +69,8 @@ export default function SelectScan() {
             return;
         }
     
-        // Prepare templateIds based on scanAllTemplates state
         const templateIds = scanAllTemplates ? [] : selectedTemplates;
-        const domainIdScanAll = templates.length > 0 ? templates[0].ID : ""; 
-    
-        // Log the values for debugging
-        console.log("Domain ID:", domainIdScanAll);
-        console.log("Template IDs:", templateIds);
+        const domainIdScanAll = templates.length > 0 ? templates[0].ID : "";
     
         try {
             const response = await fetch(`${BASE_URL}/v1/scans`, {
@@ -86,25 +79,23 @@ export default function SelectScan() {
                     'Content-Type': 'application/json',
                 },
                 body: JSON.stringify({
-                    domainIdScanAll, // use the domainId
-                    templateIds, // use selected templates or all templates based on checkbox
+                    domainIdScanAll,
+                    templateIds,
                 }),
             });
-
-            // console.log('response', response);
     
             if (response.ok) {
                 toast({
                     title: "Success",
                     description: "Scan initiated successfully.",
                 });
+                // Add a delay before redirecting
+                setTimeout(() => {
+                    router.push("/dashboard/scans");
+                }, 2000); // 2 second delay
             } else {
                 throw new Error('Failed to initiate scan');
             }
-
-            // Redirect to the scan results page after a successful scan
-            router.push("/dashboard/scans");
-
         } catch (error) {
             console.error('Error initiating scan:', error);
             toast({
@@ -115,48 +106,42 @@ export default function SelectScan() {
         }
     };    
 
-
     return (
-        <>
-            <div className="flex items-center justify-center min-h-screen bg-gray-100">
-                <div className="p-8 bg-white shadow-lg rounded-md flex-1 flex flex-col max-w-4xl">
-                    <h2 className="text-2xl font-bold mb-4">Select Scan Templates</h2>
-                    <p className="mb-4 text-gray-600">Target: {target}</p>
-                    <form onSubmit={handleSubmit} className="space-y-4 flex-grow">
-                        <div className="space-y-4">
-                            {templates.map(template => (
-                                <div key={template.ID} className="flex items-center space-x-3">
-                                    <Checkbox
-                                        id={template.ID}
-                                        checked={selectedTemplates.includes(template.ID)}
-                                        onCheckedChange={() => handleTemplateSelection(template.ID)}
-                                    />
-                                    <label htmlFor={template.ID} className="text-gray-700">{template.Name}</label>
-                                </div>
-                            ))}
-                            <div className="flex items-center space-x-3">
+        <div className="flex items-center justify-center min-h-screen bg-gray-100">
+            <div className="p-8 bg-white shadow-lg rounded-md flex-1 flex flex-col max-w-4xl">
+                <h2 className="text-2xl font-bold mb-4">Select Scan Templates</h2>
+                <p className="mb-4 text-gray-600">Target: {target}</p>
+                <form onSubmit={handleSubmit} className="space-y-4 flex-grow">
+                    <div className="space-y-4">
+                        {templates.map(template => (
+                            <div key={template.ID} className="flex items-center space-x-3">
                                 <Checkbox
-                                    id="scanAllTemplates"
-                                    checked={scanAllTemplates}
-                                    onCheckedChange={setScanAllTemplates}
+                                    id={template.ID}
+                                    checked={selectedTemplates.includes(template.ID)}
+                                    onCheckedChange={() => handleTemplateSelection(template.ID)}
                                 />
-                                <label htmlFor="scanAllTemplates" className="text-gray-700">Scan All Templates</label>
+                                <label htmlFor={template.ID} className="text-gray-700">{template.Name}</label>
                             </div>
+                        ))}
+                        <div className="flex items-center space-x-3">
+                            <Checkbox
+                                id="scanAllTemplates"
+                                checked={scanAllTemplates}
+                                onCheckedChange={setScanAllTemplates}
+                            />
+                            <label htmlFor="scanAllTemplates" className="text-gray-700">Scan All Templates</label>
                         </div>
-                        <Button
-                            type="submit"
-                            className="w-full py-2 mt-4 text-white bg-green-600 rounded-md hover:bg-green-700"
-                            disabled={selectedTemplates.length === 0 && !scanAllTemplates}
-                        >
-                            Start Scan
-                        </Button>
-                    </form>
-                </div>
-
-                <div>
-                    <Toaster />
-                </div>
+                    </div>
+                    <Button
+                        type="submit"
+                        className="w-full py-2 mt-4 text-white bg-green-600 rounded-md hover:bg-green-700"
+                        disabled={selectedTemplates.length === 0 && !scanAllTemplates}
+                    >
+                        Start Scan
+                    </Button>
+                </form>
             </div>
-        </>
+            <Toaster />
+        </div>
     );
 }
