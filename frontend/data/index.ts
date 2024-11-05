c8cdb949de07a96114fcb9c2cbdc8a09d98f674b
elvisyong <elvisyong@gmail.com>
2024-11-05 15:11:57 +0800

    bump: version

--- frontend/data/index.ts
+++ frontend/data/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// export const BASE_URL: string = "http://a75d6504ef6df43ef9136c6996396272-41456714.ap-southeast-1.elb.amazonaws.com"
-export const BASE_URL: string = "http://0.0.0.0:5000";
+export const BASE_URL: string = "http://a75d6504ef6df43ef9136c6996396272-41456714.ap-southeast-1.elb.amazonaws.com"
+// export const BASE_URL: string = "http://0.0.0.0:5000";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
